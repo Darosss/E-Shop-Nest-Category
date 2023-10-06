e6072dbff9fe33ce3ccbdb3084b1a4175fc7eb81
Darosss <daroing12@gmail.com>
2023-10-06 16:42:47 +0200

    feat: add ProcessEnv global environment types

---
+++ src/env.d.ts
@@ -0,0 +1,15 @@
+export {};
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      POSTGRES_HOST: string;
+      POSTGRES_PORT: string;
+      POSTGRES_USERNAME: string;
+      POSTGRES_PASSWORD: string;
+      POSTGRES_DATABASE: string;
+      NODE_ENV: 'development' | 'production';
+      CATEGORY_MICROSERVICE_URL: string;
+      PRODUCT_MICROSERVICE_URL: string;
+    }
+  }
+}
